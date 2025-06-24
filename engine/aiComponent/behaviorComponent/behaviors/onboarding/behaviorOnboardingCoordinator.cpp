508efd52b077b4f997e942a790898482c8e3365a
Switch-modder <deolrajjyot@gmail.com>
2025-06-24 17:24:35 -0400

    Now onboarding should play wakeup?

--- engine/aiComponent/behaviorComponent/behaviors/onboarding/behaviorOnboardingCoordinator.cpp
+++ engine/aiComponent/behaviorComponent/behaviors/onboarding/behaviorOnboardingCoordinator.cpp
@@ -334,10 +334,11 @@ void BehaviorOnboardingCoordinator::HandleOnboardingMessageFromApp(const AppToEn
     }
     case AppToEngineTag::kOnboardingMarkCompleteAndExit:
     {
-      _dVars.markCompleteAndExitOnNextUpdate = true;
       // Hack to make it work until we can force websetup
       // to play wakeup and then exit.
       _dVars.pendingPhase = OnboardingPhase::WakeUp;
+
+      _dVars.shouldExitAfterWakeUp = true; // Used somewhere down the lines of code
       break;
     }
     case AppToEngineTag::kAppDisconnected:
@@ -532,6 +533,13 @@ void BehaviorOnboardingCoordinator::OnPhaseComplete(const OnboardingPhase& phase
   LOG_INFO("BehaviorOnboardingCoordinator.OnPhaseComplete",
            "Completed onboarding phase %s. Idling.",
            EnumToString(phase));
+
+  // This should kill onboarding once it's done?
+  if (_dVars.shouldExitAfterWakeUp && OnboardingPhase::WakeUp == phase) {
+      _dVars.markCompleteAndExitOnNextUpdate = true;
+      _dVars.shouldExitAfterWakeUp = false;
+      return; 
+  }
 
   if( _dVars.emulate1p0Onboarding ){
     if( OnboardingPhase::WakeUp == phase ){
